024b2c1ac3d1857e93afa6d3e7ae3d67220e51c8
subhashmaramreddy <subhashmaramreddy@gmail.com>
2026-04-23 10:04:00 +0530

    fix cors properly

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,9 +17,7 @@ const httpServer = createServer(app);
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: "*"
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
